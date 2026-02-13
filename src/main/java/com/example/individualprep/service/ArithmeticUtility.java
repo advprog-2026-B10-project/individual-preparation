008ed913d5343043fdf07f15b3ee1fc5e09e59cd
Ahmad Wasis <ahmadwasissh@gmail.com>
2026-02-13 15:28:54 +0700

    implement arithmetic add method

--- src/main/java/com/example/individualprep/service/ArithmeticUtility.java
+++ src/main/java/com/example/individualprep/service/ArithmeticUtility.java
@@ -7,7 +7,7 @@ public class ArithmeticUtility {
     
     public double add(double o1, double o2) {
         // TODO: Implement me properly!
-        return 0.0;
+        return o1 + o2;
     }
 
     public double subtract(double o1, double o2) {
